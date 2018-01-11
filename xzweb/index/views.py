bacc6f5381beb75ff8f8fb193cd6b7330437b936
carson <carson@qq.com>
2018-01-11 13:38:31 +0800

    change

--- xzweb/index/views.py
+++ xzweb/index/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 
-# Create your views here.
+# 首页
 def index(request):
     return render(request, 'index.html')
-
+#登录页面
 def page_login(request):
     return render(request, 'page_login.html')
-
+#注册页面
 def page_registration(request):
     return render(request, 'page_registration.html')
